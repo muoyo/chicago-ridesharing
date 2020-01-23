6edcf86d56d7a3ce694864b5850692198b3fd826
mokome <mokome@gmail.com>
2020-01-23 12:52:22 -0500

    More detailed commentary

--- python_files/data_cleaning.py
+++ python_files/data_cleaning.py
@@ -15,11 +15,18 @@ from datetime import datetime
 def merge_weather (df):
     """ Merge weather data into dataset """
     
+    # Read in weather data from CSV 
+    # (425 days of hourly weather data from Chicago, obtained via the Dark Sky API)
+    
     weather_df = pd.read_csv('../data/chicago_weather.csv')
+    
+    # Create the start_date_plus_hour column, used to merge weather data with trip data
     weather_df['hour'] = weather_df['hour'].apply(lambda x: '{:02d}'.format(x))
     weather_df['start_date_plus_hour'] = pd.to_datetime(weather_df['date'] + ' ' + weather_df['hour'] + ':00:00')
     weather_df = weather_df.rename(columns={'icon': 'precip'})
 
+    # Uncomment below code if we want to limit precipitation options to rain, snow, and clear only (see Dark Sky API)
+    
     # def set_precip(precip):
     #     if precip not in ['rain', 'snow']: 
     #         precip = 'clear'
@@ -36,6 +43,8 @@ def merge_weather (df):
 
 def clean_columns(df):
     
+    # Trim data to the columns we are interested in, listed below
+    
     columns_to_use = ['trip_id', 'trip_start_timestamp', 'trip_end_timestamp', 'trip_seconds',
        'trip_miles', 'pickup_community_area', 'fare', 'tip',
        'additional_charges', 'trip_total' ]
@@ -43,6 +52,8 @@ def clean_columns(df):
     columns_to_drop = [ col for col in df.columns if col not in columns_to_use ]
     df = df.drop(columns=columns_to_drop)
 
+    # Convert numeric & timestamp data from strings to the appropriate datatypes
+    
     df['trip_start_timestamp'] = pd.to_datetime(df['trip_start_timestamp'])
     df['trip_end_timestamp'] = pd.to_datetime(df['trip_end_timestamp'])
     df['trip_seconds'] = df['trip_seconds'].fillna('0')
@@ -51,10 +62,15 @@ def clean_columns(df):
     for col in ['trip_miles', 'fare', 'tip', 'additional_charges', 'trip_total']:
         df[col] = df[col].astype(float) 
 
+    # Create columns for the weekday, hour, and time block when the trip was initiated
+    # There are 8 three hour time blocks in each 24 hour day, with block 0 starting at 12AM
+    
     df['start_weekday'] = df['trip_start_timestamp'].apply(lambda d: d.weekday())
     df['start_hour'] = df['trip_start_timestamp'].apply(lambda d: d.hour)
     df['start_time_block'] = df['start_hour'] // 3
 
+    # We use the start_date_plus hour column to merge trip data with weather data at the hourly level
+    
     df['start_date_plus_hour'] = df['trip_start_timestamp'].apply(lambda d: datetime(d.year, d.month, d.day, d.hour))    
     
     return df
@@ -62,7 +78,7 @@ def clean_columns(df):
 
 def clean_data(df):
     """
-    This is the one function called that will run all the support functions.
+    This function runs our support functions to clean and merge the data before returning a final dataframe for analysis
     
     :return: cleaned dataset to be passed to hypothesis testing and visualization modules.
     """
